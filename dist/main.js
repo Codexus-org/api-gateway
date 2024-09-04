e7282a2600afbd958e029851d1a38b07d31be84a
mahaputrailhamawal <mahaputrailhamawal@gmail.com>
2024-09-04 08:44:32 +0700

    modified: main.ts

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_proxy_middleware_1 = require("http-proxy-middleware");
 const app = (0, express_1.default)();
-app.get("/", (_, res) => res.json({ message: "Hello World from API-Gateway" }));
 app.use("/users-services", (0, http_proxy_middleware_1.createProxyMiddleware)({ target: "http://108.136.118.28:8002/forumapp/api/v1/users-services" }));
 app.use("/threads-services", (0, http_proxy_middleware_1.createProxyMiddleware)({ target: "http://localhost:8001" }));
 app.use("/replies-services", (0, http_proxy_middleware_1.createProxyMiddleware)({ target: "http://localhost:8000" }));
